9c51bfef07cb93e1663758ae5551d93d2637047b
Your Name <you@example.com>
2024-06-25 15:44:06 +0300

    added

---
+++ lesson2/js/part2/script.js
@@ -0,0 +1,10 @@
+function task1() {
+    let x = document.getElementById('answer1').value;
+
+    if (x == 100) {
+        alert("Correct answer");
+    } else {
+        alert("Incorrect answer");
+    }
+}
+
