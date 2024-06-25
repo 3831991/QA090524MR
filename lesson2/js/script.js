030f34f24ec60ee700f59d358bf3934232fe004b
Your Name <you@example.com>
2024-06-25 12:59:09 +0300

    added

---
+++ lesson2/js/script.js
@@ -0,0 +1,12 @@
+function greet() {
+    alert("ברוכים הבאים!");
+}
+
+// פונקציה עם אנימציה בגלל הסקרנות
+function greet2() {
+    document.getElementById('msg').style.display = 'flex';
+
+    setTimeout(() => {
+        document.getElementById('msg').style.display = 'none';
+    }, 2000);
+}
