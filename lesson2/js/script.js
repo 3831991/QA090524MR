7d0ddb03640bff10ca67780e156eeb669cfdd267
Your Name <you@example.com>
2024-06-25 15:15:57 +0300

    added

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -19,3 +19,20 @@ function greetName() {
     let un = document.getElementById('userName').value;
     alert("ברוך הבא " + un);
 }
+
+function multi50() {
+    let n = document.getElementById('num1').value;
+
+    alert(n * 50);
+}
+
+function nulti() {
+    let n1 = document.getElementById('num2').value;
+    let n2 = document.getElementById('num3').value;
+
+    alert(n1 * n2);
+}
+
+function sum() {
+    
+}
